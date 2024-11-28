ab70c20f763f86b7c7cfd0b95c11e551cccbd5f8
arisson.simon <arisson.simon@satc.edu.br>
2024-11-28 10:25:04 -0300

    adicionado confit ts para compilar em dist

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.multiply = multiply;
-function multiply(a, b) {
-    return a * b;
-}
-console.log(multiply(2, 15));
